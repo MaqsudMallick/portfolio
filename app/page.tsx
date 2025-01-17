d5f62038b79c7aeafeeac25b85190e710c891514
Maqsud <maksud@zendot.in>
2025-01-17 17:46:57 +0530

    avik

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Suspense fallback={<div>Loading...</div>}>
-        {/* <FloatingNav navItems={navItems} /> */}
+        <FloatingNav navItems={navItems} />
         {/* <Hero /> */}
         {/* <Grid /> */}
         {/* <RecentProjects /> */}
